eaf7ae577f3bcf7c8acd99b6ab916e32747da982
niyou0ct <axaebyxo11@gmail.com>
2020-09-05 10:16:46 +0900

    fix: mail error

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -21,45 +21,45 @@ const transporter = nodemailer.createTransport({
  *
  * @param params
  */
-// const sendEmailToAdmin = async (params: Contact): Promise<any> => {
-//   const { email, name, detail } = params
+const sendEmailToAdmin = async (params: Contact): Promise<any> => {
+  const { email, name, detail } = params
 
-//   const message = {
-//     from: process.env.EMAIL_ADDRESS,
-//     to: process.env.EMAIL_ADDRESS,
-//     subject: `お問い合わせがありました`,
-//     text: `■メールアドレス\n${email}\n\n■お名前\n${name}\n\n■お問い合わせ内容\n${detail}`
-//   }
+  const message = {
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: `お問い合わせがありました`,
+    text: `■メールアドレス\n${email}\n\n■お名前\n${name}\n\n■お問い合わせ内容\n${detail}`
+  }
 
-//   const response = await transporter
-//     .sendMail(message)
-//     .then((res) => res)
-//     .catch((err) => err)
-//   return response
-// }
+  const response = await transporter
+    .sendMail(message)
+    .then((res) => res)
+    .catch((err) => err)
+  return response
+}
 
-// /**
-//  *
-//  * @param params
-//  */
-// const sendEmailToClient = async (params: Contact): Promise<any> => {
-//   const { email, name, detail } = params
-//   const greetingText =
-//     'この度はお問い合わせ誠にありがとうございます。自動でお送りするメールのためこちらへの返信は不要です。\n=========================\nお問い合わせしていただいた内容\n=========================\n'
+/**
+ *
+ * @param params
+ */
+const sendEmailToClient = async (params: Contact): Promise<any> => {
+  const { email, name, detail } = params
+  const greetingText =
+    'この度はお問い合わせ誠にありがとうございます。自動でお送りするメールのためこちらへの返信は不要です。\n=========================\nお問い合わせしていただいた内容\n=========================\n'
 
-//   const message = {
-//     from: process.env.EMAIL_ADDRESS,
-//     to: email,
-//     subject: `Nie's Digitalへお問い合わせありがとうございます`,
-//     text: `${greetingText}■メールアドレス\n${email}\n\n■お名前\n${name}\n\n■お問い合わせ内容\n${detail}`
-//   }
+  const message = {
+    from: process.env.EMAIL_ADDRESS,
+    to: email,
+    subject: `Nie's Digitalへお問い合わせありがとうございます`,
+    text: `${greetingText}■メールアドレス\n${email}\n\n■お名前\n${name}\n\n■お問い合わせ内容\n${detail}`
+  }
 
-//   const response = await transporter
-//     .sendMail(message)
-//     .then((res) => res)
-//     .catch((err) => err)
-//   return response
-// }
+  const response = await transporter
+    .sendMail(message)
+    .then((res) => res)
+    .catch((err) => err)
+  return response
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async (
@@ -76,16 +76,16 @@ export default async (
 
   const transporterRes: boolean = await transporter.verify()
 
-  // if (!transporterRes) {
-  //   res.json({ result: false, clientResult: undefined, adminResult: undefined })
-  // }
+  if (!transporterRes) {
+    res.json({ result: false, clientResult: undefined, adminResult: undefined })
+  }
 
-  // const clientRes = await sendEmailToClient(jsonParams)
-  // const adminRes = await sendEmailToAdmin(jsonParams)
+  const clientRes = await sendEmailToClient(jsonParams)
+  const adminRes = await sendEmailToAdmin(jsonParams)
 
   res.json({
     result: true,
-    clientResult: transporterRes,
-    adminResult: jsonParams
+    clientResult: clientRes,
+    adminResult: adminRes
   })
 }
